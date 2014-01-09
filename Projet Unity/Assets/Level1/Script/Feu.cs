c77bf3ad0d0cd897a33e4bb0003c113b0e2273e3
Thenkei <morganpo30@gmail.com>
2014-01-09 07:05:46 +0100

    Ajouts feu et tout
    Brule jeu. D:

---
+++ Projet Unity/Assets/Level1/Script/Feu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Feu : MonoBehaviour {
+
+	private CharacterCaracteristics PlayerCaract;
+
+	private GameObject Text;
+
+	void OnTriggerStay(Collider other) {
+		PlayerCaract = GameObject.FindGameObjectWithTag("Player").GetComponent("CharacterCaracteristics") as CharacterCaracteristics;
+		PlayerCaract.SetDeltaVitality(-1);
+
+		Text =  Instantiate(Resources.Load("Prefab/Text"),new Vector3(0.41f,0.2f,0f), Quaternion.identity) as GameObject;
+		
+		Text.guiText.text = "Brulure";
+		Text.guiText.color = new Color32(255,12,56,200);
+	}
+}
